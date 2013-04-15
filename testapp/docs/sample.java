e05b2cd65a48026fdc3492c98c744d9388289a20
prathamesh <pratham_920@yahoo.com>
2013-04-15 12:40:22 +0530

    Sample instructor code and test-code arguments

---
+++ testapp/docs/sample.java
@@ -0,0 +1,8 @@
+class sample 
+{
+        public static void main(String arg[])
+        {
+                int sum =Integer.parseInt(arg[0])+Integer.parseInt(arg[1]);
+                System.out.print(sum);
+        }
+}
